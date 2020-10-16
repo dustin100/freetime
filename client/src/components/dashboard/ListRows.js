2e9f98059c8aa8d849db4b73359859c0614ea497
Dustin <dmkelly100@gmail.com>
2020-10-16 14:50:30 -0400

    updated css for list component

--- client/src/components/dashboard/ListRows.js
+++ client/src/components/dashboard/ListRows.js
@@ -8,7 +8,7 @@ const ListRows = ({ list, title, clicked }) => {
 		infinite: false,
 		speed: 500,
 		slidesToShow: 1,
-		slidesToScroll: 1,
+		slidesToScroll: 2,
 		accessibility: true,
 		variableWidth: true,
 		rows: 1,
@@ -20,7 +20,7 @@ const ListRows = ({ list, title, clicked }) => {
 
 	const listRow = list.map(({ title, release, url, _id }) => {
 		return (
-			<div key={_id}>
+			<div className='card-wrapper' key={_id}>
 				<div className='card'>
 					<div className='cardTop'>
 						<h2>{title}</h2>
